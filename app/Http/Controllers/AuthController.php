b1f98472a7e3943661eacd276764f5afd82de0ea
ENX1038 <149946487+EN1038@users.noreply.github.com>
2024-12-11 16:04:41 +0700

    update login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,6 +14,6 @@ class AuthController extends Controller
 
     public function RegisterPage()
     {
-        return view('auth.login');
+        return view('auth.register');
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,41 @@
-@extends('dashboard.layout.users.layout_users')
-@section('user_content')
+@extends('auth.layouts.layout-login-register')
+@section('title', 'Login')
+
+@section('content')
 
 <div class="container">
-    <h1>login</h1>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card shadow-sm">
+                <div class="card-header text-center bg-primary text-white">Login</div>
+                <div class="card-body">
+                    <form method="POST" action="">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="email" class="form-label">{{ __('Email Address') }}</label>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autofocus>
+                            @error('email')
+                                <span class="text-danger small">{{ $message }}</span>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="password" class="form-label">{{ __('Password') }}</label>
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required>
+                            @error('password')
+                                <span class="text-danger small">{{ $message }}</span>
+                            @enderror
+                        </div>
+
+                        <a href="{{route('RegisterPage')}}">register</a>
+                        <div class="d-grid">
+                            <button type="submit" class="btn btn-primary">{{ __('Login') }}</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection
